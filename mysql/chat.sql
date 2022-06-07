070903dd1f526e85c2e9d62faad885517badfc4b
zhuwenboa <879658730@qq.com>
2022-06-07 19:44:22 +0800

    建表

---
+++ mysql/chat.sql
@@ -0,0 +1,7 @@
+```好友关系表```
+CREATE TABLE `friend` (
+  `name1` varchar(20) DEFAULT NULL,
+  `name2` varchar(20) DEFAULT NULL,
+  `status` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
